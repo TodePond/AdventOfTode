0e2c835765d5a96e5113f40429a29774c10e0e28
Lu[ke] Wilson <l2wilson94@gmail.com>
2022-12-03 18:32:05 +0000

    day 3b

--- 2022/day03b/script.js
+++ 2022/day03b/script.js
@@ -12,23 +12,24 @@ for (let i = 0; i < ITEMS.length; i++) {
 	PRIORITIES[item] = i
 }
 
-// Rucksacks are made up of two equal length containers
-const rucksacks = input.split("\n").map((line) => {
-	const container = line.split("")
-	const left = container.slice(0, container.length / 2)
-	const right = container.slice(container.length / 2)
-	return [left, right]
-})
+const rucksacks = input.split("\n").map((v) => v.split(""))
 
-// Find any items that appear in both containers
-const duplicates = rucksacks
-	.map(([left, right]) => {
-		const leftSet = new Set(left)
-		const rightSet = new Set(right)
-		const intersection = new Set([...leftSet].filter((x) => rightSet.has(x)))
-		return [...intersection]
-	})
-	.flat(Infinity)
+// Rucksacks are grouped together in threes
+const groups = rucksacks.reduce((acc, v, i) => {
+	const groupIndex = Math.floor(i / 3)
+	if (!acc[groupIndex]) acc[groupIndex] = []
+	acc[groupIndex].push(v)
+	return acc
+}, [])
+
+// Find any items that appear in all three containers
+const duplicates = groups.map(([left, middle, right]) => {
+	const leftSet = new Set(left)
+	const middleSet = new Set(middle)
+	const rightSet = new Set(right)
+	const intersection = new Set([...leftSet].filter((x) => middleSet.has(x) && rightSet.has(x)))
+	return [...intersection]
+})
 
 const priorities = duplicates.map((item) => PRIORITIES[item])
 const sum = priorities.reduce((a, b) => a + b, 0)
