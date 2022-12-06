e2d2877e223843655ef9d2c707b3835fcd02cf10
Lu[ke] Wilson <l2wilson94@gmail.com>
2022-12-06 19:47:19 +0000

    prep day 5b

---
+++ 2022/day05b/script.js
@@ -0,0 +1,90 @@
+const input = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`
+
+const { Term } = MotherTode
+
+const [head, tail] = input.split("\n\n")
+const instructions = tail.split("\n")
+const layers = head.split("\n").slice(0, -1)
+
+const Instruction = Term.hoist(({ move, from, to, number, instruction }) => {
+	return {
+		move: Term.string("move "),
+		from: Term.string(" from "),
+		to: Term.string(" to "),
+		number: Term.emit(Term.regExp(/[0-9]+/), (v) => parseInt(v)),
+
+		instruction: Term.emit(Term.list([move, number, from, number, to, number]), ([m, count, f, start, t, end]) => ({
+			count,
+			start: start - 1,
+			end: end - 1,
+		})),
+	}
+})
+
+const Arrangement = Term.hoist(({ gap, cell, empty, crate, row, rowTail }) => {
+	return {
+		gap: Term.string(" "),
+		cell: Term.or([crate, empty]),
+		empty: Term.emit(Term.string("   "), () => ""),
+		crate: Term.emit(
+			Term.list([Term.string("["), Term.regExp(/[A-Z]/), Term.string("]")]),
+			([_, letter]) => letter,
+		),
+
+		row: Term.emit(Term.list([cell, Term.maybe(rowTail)]), ([first, rest]) => {
+			return [first, ...(rest || [])]
+		}),
+
+		rowTail: Term.emit(Term.list([gap, row]), ([_, rest]) => rest),
+	}
+})
+
+const movements = instructions.map((v) => Instruction.instruction.translate(v)).d
+const rows = layers.map((v) => Arrangement.row.translate(v))
+
+const startingColumns = rows[0].map((_, i) =>
+	rows
+		.map((row) => row[i])
+		.reverse()
+		.filter((v) => v !== ""),
+).d
+
+const applyMovement = (columns, movement) => {
+	const { start, end, count } = movement
+
+	const newColumns = [...columns]
+
+	const startColumn = newColumns[start]
+	const endColumn = newColumns[end]
+
+	const selectedCrates = startColumn.slice(-count).reverse()
+	const remainingCrates = startColumn.slice(0, -count)
+
+	const newStartColumn = [...remainingCrates]
+	const newEndColumn = [...endColumn, ...selectedCrates]
+
+	newColumns[start] = newStartColumn
+	newColumns[end] = newEndColumn
+
+	return newColumns
+}
+
+const applyMovements = (columns, movements) => {
+	let newColumns = columns
+	for (const movement of movements) {
+		newColumns = applyMovement(newColumns, movement)
+	}
+	return newColumns
+}
+
+const endingColumns = applyMovements(startingColumns, movements).d
+
+const endingTops = endingColumns.map((column) => column.at(-1) || "").join("").d
